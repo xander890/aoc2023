c4b3e3309c1f50c3fbed4bc014d4c74046e2de0e
Alessandro Dal Corso <xander890@gmail.com>
2023-12-04 20:53:01 +0100

    faster :)

--- problem4.py
+++ problem4.py
@@ -7,10 +7,10 @@ for n, line in enumerate(lines):
     sepBar = line.index('|')
     win = line[sepCol+1:sepBar].split()
     play = line[sepBar:].split()
-    wins = len([x for x in win if x in play])
+    wins = len(set(win).intersection(set(play)))
     points += int(2**(wins - 1))
     for i in range(wins):
         cardCount[n + i + 1] += cardCount[n]
 
 print("part1:", points)
-print("part2:", sum(cardCount))    
+print("part2:", sum(cardCount))
