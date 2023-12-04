55d9919cc1fe30633b3bff5f0f390c56e3822d5d
Alessandro Dal Corso <xander890@gmail.com>
2023-12-04 20:59:18 +0100

    more pythonic

--- problem4.py
+++ problem4.py
@@ -1,13 +1,13 @@
 file = open('input4.txt', 'r')
 lines = file.readlines()
-cardCount = [1 for x in range(len(lines))]
+cardCount = [1] * len(lines)
 points = 0
 for n, line in enumerate(lines):
     sepCol = line.index(':')
     sepBar = line.index('|')
     win = line[sepCol+1:sepBar].split()
     play = line[sepBar:].split()
-    wins = len(set(win).intersection(set(play)))
+    wins = len(set(win) & set(play))
     points += int(2**(wins - 1))
     for i in range(wins):
         cardCount[n + i + 1] += cardCount[n]
